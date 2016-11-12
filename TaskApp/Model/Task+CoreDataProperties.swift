a325d5b2774b6897c1c15322c768db7424055317
mokemoko <mokemoko331@gmail.com>
2016-11-12 11:58:44 +0900

    add magical record

---
+++ TaskApp/Model/Task+CoreDataProperties.swift
@@ -0,0 +1,19 @@
+//
+//  Task+CoreDataProperties.swift
+//  TaskApp
+//
+//  Created by Teacher on 2016/11/12.
+//  Copyright © 2016年 AKYLab. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension Task {
+
+    @NSManaged var name: String?
+
+}
---
+++ TaskApp/Model/Task.swift
@@ -0,0 +1,17 @@
+//
+//  Task.swift
+//  TaskApp
+//
+//  Created by Teacher on 2016/11/12.
+//  Copyright © 2016年 AKYLab. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(Task)
+class Task: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
